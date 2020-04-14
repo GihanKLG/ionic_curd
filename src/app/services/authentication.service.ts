8439ceb0eb6f47c7d15cedc0171ae7205f41bfe5
GihanDarshana <45061133+GihanDarshana@users.noreply.github.com>
2020-04-14 11:54:46 +0530

    modify login function

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -2,6 +2,7 @@ import { Platform } from '@ionic/angular';
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { BehaviorSubject } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
  
 const TOKEN_KEY = 'auth-token';
  
@@ -11,8 +12,9 @@ const TOKEN_KEY = 'auth-token';
 export class AuthenticationService {
  
   authenticationState = new BehaviorSubject(false);
+  session_id: any;
  
-  constructor(private storage: Storage, private plt: Platform) { 
+  constructor(private storage: Storage, private plt: Platform, private http: HttpClient) { 
     this.plt.ready().then(() => {
       this.checkToken();
     });
@@ -26,16 +28,22 @@ export class AuthenticationService {
     })
   }
  
-  login() {
-    return this.storage.set(TOKEN_KEY, 'Bearer 1234567').then(() => {
-      this.authenticationState.next(true);
-    });
+  login(url) {
+    //return this.storage.set(TOKEN_KEY, 'Bearer 1234567').then(() => {
+      console.log(url);
+      this.http.get('http://localhost/60plus/svr/login.php?user=yakandawala&pass=test123').subscribe((res:any) => {
+        this.session_id = res.details.session_id;
+        // console.log(this.session_id);
+        this.authenticationState.next(true);
+      });
+      
+   // });
   }
  
   logout() {
-    return this.storage.remove(TOKEN_KEY).then(() => {
+   // return this.storage.remove(TOKEN_KEY).then(() => {
       this.authenticationState.next(false);
-    });
+   // });
   }
  
   isAuthenticated() {

c0086840acc00beda889ab6f9b2d403fd2d0142d
GihanDarshana <45061133+GihanDarshana@users.noreply.github.com>
2020-04-16 22:51:59 +0530

    add api call into heat map

--- src/app/googleheatmap/googleheatmap.page.ts
+++ src/app/googleheatmap/googleheatmap.page.ts
@@ -3,6 +3,7 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
 import { MarkerCluster } from "@ionic-native/google-maps";
 import { AuthenticationService } from './../services/authentication.service';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 
 declare var google;
 declare var MarkerClusterer:any;
@@ -26,7 +27,7 @@ export class GoogleheatmapPage {
 
   constructor(
     private geolocation: Geolocation,
-    private nativeGeocoder: NativeGeocoder, private authService: AuthenticationService) {
+    private nativeGeocoder: NativeGeocoder, private authService: AuthenticationService, private http: HttpClient) {
   }
 
 
@@ -77,69 +78,30 @@ export class GoogleheatmapPage {
     
      var url = 'http://localhost/googlemap/svr/report.php?action=read&session_id=' + id;
      console.log(url);
-    });
- 
-      var heatmapData = [
-        new google.maps.LatLng(6.959515, 80.603027),
-        new google.maps.LatLng(8.243478, 81.131043),
-        new google.maps.LatLng(8.234277, 81.125274),
-        new google.maps.LatLng(8.209265, 81.105157),
-        new google.maps.LatLng(8.233463, 81.125672),
-        new google.maps.LatLng(6.165987, 80.271010),
-        new google.maps.LatLng(6.151515, 80.292345),
-        new google.maps.LatLng(8.235683, 81.125802),
-        new google.maps.LatLng(8.201049, 81.111349),
-        new google.maps.LatLng(8.233326, 81.125724),
-        new google.maps.LatLng(8.225308, 81.124102),
-        new google.maps.LatLng(8.244872, 81.125304),
-        new google.maps.LatLng(6.450889, 80.038218),
-        new google.maps.LatLng(6.173025, 80.203362),
-        new google.maps.LatLng(6.151114, 80.273284),
-        new google.maps.LatLng(6.431923, 80.375463),
-        new google.maps.LatLng(6.364028, 80.38032),
-        new google.maps.LatLng(6.431923, 80.375464),
-        new google.maps.LatLng(6.364028, 80.38033)
-        
-      ];
+
+     this.http.get(url).subscribe((res:any) => { 
       
+      var location = res.details.Location;
+      var i;
+      var heatmapData = [];
+
+      for(i=0;i<location.length;i++) {
+        var lat = Number(location[i].lat);
+        var lng = Number(location[i].lng);
+
+        heatmapData[i] = new google.maps.LatLng(lat, lng);
+        //console.log(heatmapData[i]);
+      } 
+
       var pointArray = new google.maps.MVCArray(heatmapData);
       var heatmap = new google.maps.visualization.HeatmapLayer({
         data: pointArray
       });
       heatmap.setMap(this.map);
-      
-    // let points = [
-    //   {lat: 6.959515, lng: 80.603027}, // north west
-    //   {lat: 6.969550, lng: 80.603027}, // south west
-    //   {lat: 6.969550, lng: 80.614030}, // south east
-    //   {lat: 6.959515, lng: 80.614030}  // north east
-    // ];
 
-    // var polyline = new google.maps.Polygon({
-    //   map: this.map,
-    //   path: points,
-    //   'color' : '#AA00FF',
-    //   'width': 10,
-    //   'geodesic': true
-    // });
+    });
 
-    // let points1 = [
-    //   {lat: 6.859515, lng: 80.603027}, // north west
-    //   {lat: 6.869550, lng: 80.603027}, // south west
-    //   {lat: 6.869550, lng: 80.614030}, // south east
-    //   {lat: 6.859515, lng: 80.614030}  // north east
-    // ];
-
-    // var polyline1 = new google.maps.Polygon({
-    //   map: this.map,
-    //   path: points,
-    //   'color' : '#AA00FF',
-    //   'width': 10,
-    //   'geodesic': true
-    // });
- 
-
+  });
   }
-
 
 }
